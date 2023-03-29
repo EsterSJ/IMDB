e4f803ff506a4c6b5b8f43866214ea956a28fa99
EsterSJ <ester_s@hotmail.com>
2023-03-29 11:42:34 +0200

    ultima modificacion

--- imdb.ts
+++ imdb.ts
@@ -15,9 +15,7 @@ class Imdb{
 
     public obtenerInstanciaIMDB(nombreFichero:string): Imdb{
         let newImdb: Imdb = new Imdb([]);
-        newImdb.peliculas = JSON.parse(readFileSync("imdbBBDD.json").toString()).peliculas;
-        console.log(typeof newImdb);
-        
+        newImdb.peliculas = JSON.parse(readFileSync("imdbBBDD.json").toString()).peliculas;        
         return newImdb;
     }
 }
